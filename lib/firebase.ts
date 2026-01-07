0b674cbc323f87064ebd330b1de86aca20fe29ce
gabrielldjr-creator <gabrielldjr@gmail.com>
2026-01-07 19:11:52 -0300

    Remove unused access components for pilot

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,6 @@
 "use client";
 
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -16,4 +15,3 @@ const firebaseConfig = {
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const auth = getAuth(app);
